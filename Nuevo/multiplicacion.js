e87e349939e3f7aafca379457a856d1c7c8651a0
Dinodani2006 <114485372+Dinodani2006@users.noreply.github.com>
2023-03-20 11:16:23 +0100

    Add files via upload

---
+++ Nuevo/multiplicacion.js
@@ -0,0 +1,8 @@
+function multiplicacion(a,b){
+    return a*b;
+        }
+
+        const resultado = multiplicacion(3,5);
+    
+    console.log("la multiplicacion de estos numeros es: " + resultado);
+    
---
+++ Nuevo/saludo.js
@@ -0,0 +1,8 @@
+function saludo(a){
+    return "Bienvenido " + a;
+        }
+        let dani = ""
+let mensaje = saludo(dani);
+    
+console.log(mensaje);
+    
