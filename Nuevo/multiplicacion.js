d018825c3fab6ce11c3583a8016086a5272b9670
Dinodani2006 <114485372+Dinodani2006@users.noreply.github.com>
2023-03-21 13:06:42 +0100

    Delete multiplicacion.js

--- Nuevo/multiplicacion.js
+++
@@ -1,8 +0,0 @@
-function multiplicacion(a,b){
-    return a*b;
-        }
-
-        const resultado = multiplicacion(3,5);
-    
-    console.log("la multiplicacion de estos numeros es: " + resultado);
-    
